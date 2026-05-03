c1e6250668427a3aaa59b6e81f626534d79633da
KamiKiseki <michaellocus7@gmail.com>
2026-05-02 20:17:56 -0500

    fix port to use Railway PORT env var

--- server.py
+++ server.py
@@ -1127,4 +1127,5 @@ def health():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
